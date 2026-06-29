b6a69e8c81438dff4c5c2010cf43eb26fb57a801
JoergSW <Traden123@t-online.de>
2026-06-29 18:21:12 +0200

    Integrate runtime kernel services

--- src/trading_platform/kernel/application.py
+++ src/trading_platform/kernel/application.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+from trading_platform.kernel.command_bus import CommandBus
 from trading_platform.kernel.dependency_container import DependencyContainer
+from trading_platform.kernel.event_bus import EventBus
+from trading_platform.kernel.query_bus import QueryBus
+from trading_platform.kernel.runtime_context import RuntimeContext
 
 
 class Application:
@@ -8,11 +12,38 @@ class Application:
 
     def __init__(self) -> None:
         self._container = DependencyContainer()
+        self._runtime = RuntimeContext()
+
+        self._event_bus = EventBus()
+        self._command_bus = CommandBus()
+        self._query_bus = QueryBus()
+
         self._running = False
+
+        self._container.register_singleton("runtime", self._runtime)
+        self._container.register_singleton("event_bus", self._event_bus)
+        self._container.register_singleton("command_bus", self._command_bus)
+        self._container.register_singleton("query_bus", self._query_bus)
 
     @property
     def container(self) -> DependencyContainer:
         return self._container
+
+    @property
+    def runtime(self) -> RuntimeContext:
+        return self._runtime
+
+    @property
+    def event_bus(self) -> EventBus:
+        return self._event_bus
+
+    @property
+    def command_bus(self) -> CommandBus:
+        return self._command_bus
+
+    @property
+    def query_bus(self) -> QueryBus:
+        return self._query_bus
 
     @property
     def is_running(self) -> bool:
@@ -24,6 +55,4 @@ class Application:
         self._running = True
 
     def stop(self) -> None:
-        if not self._running:
-            return
         self._running = False
