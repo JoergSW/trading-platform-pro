1159ea2b4d37143092f09bbfa5b0da072ee6c230
JoergSW <Traden123@t-online.de>
2026-06-29 18:06:39 +0200

    Refactor application lifecycle

--- src/trading_platform/kernel/application.py
+++ src/trading_platform/kernel/application.py
@@ -1,7 +1,29 @@
-from .container import Container
+from __future__ import annotations
+
+from trading_platform.kernel.dependency_container import DependencyContainer
+
+
 class Application:
-    def __init__(self):
-        self.container=Container()
-        self.running=False
-    def start(self): self.running=True
-    def stop(self): self.running=False
+    """Root application object."""
+
+    def __init__(self) -> None:
+        self._container = DependencyContainer()
+        self._running = False
+
+    @property
+    def container(self) -> DependencyContainer:
+        return self._container
+
+    @property
+    def is_running(self) -> bool:
+        return self._running
+
+    def start(self) -> None:
+        if self._running:
+            raise RuntimeError("Application is already running.")
+        self._running = True
+
+    def stop(self) -> None:
+        if not self._running:
+            return
+        self._running = False
